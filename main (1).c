08d9ba4ebb31899b39c1f0dde2fc2f2796e4cc29
Kushagra Raghav <152775429+Kushagraraghav@users.noreply.github.com>
2024-02-01 09:37:35 +0530

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,15 @@
+
+// Online C Compiler - Build, Compile and Run your C programs online in your favorite browser
+
+#include<stdio.h>
+
+int main() {
+   for (int i =1;i<=5;i++)
+   {
+       for(int j = 1;j<=i;j++)
+       {
+           printf("+");
+       }
+       printf("\n");
+   }
+}
